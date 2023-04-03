e6380ea3a230c83b5e28d79e98f06ed7644bd276
mohamedabdimohammud <mmohammud@outlook.com>
2023-04-03 10:15:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  word = word.toLowerCase();
+  word = word.replace(/[^a-z0-9]/g, "");
+  const reversedWord = word.split("").reverse().join("");
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
-  Add your pseudocode here
+1. Convert the word to lowercase
+2. Remove non-alphanumeric characters using a regex
+3. Check if the cleaned word is the same reversed
 */
 
 /*
-  Add written explanation of your solution here
+The function takes a word as an argument and checks if it is a palindrome. To do this, it first converts the word to lowercase and removes all non-alphanumeric characters using a regular expression. It then checks if the cleaned word is the same when reversed. If it is, the function returns true, otherwise it returns false. 
+
+The pseudocode outlines the steps the function takes to determine if a word is a palindrome. The first step is to convert the word to lowercase to make it case-insensitive. The second step removes all non-alphanumeric characters using a regular expression. Finally, the function checks if the cleaned word is the same when reversed. 
 */
 
-// You can run `node index.js` to view these console logs
+// Add your own custom tests below this line
+
 if (require.main === module) {
-  // add your own custom tests in here
+  // Run the test cases
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
-}
 
-module.exports = isPalindrome;
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
+}
